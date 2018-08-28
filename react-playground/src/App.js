944be21ceb31e85728527cb55ec3ffd182dabe9d
Philip Riecks <philip@riecks.io>
2018-08-28 17:14:04 +0200

    add example for using classnames package with React component

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -2,10 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Timer from './components/Timer';
+import classNames from 'classnames';
 
 class App extends React.Component {
 
+  state = {
+    isAlert: true,
+    isDangerous: false
+  }
+
+  changeColor = () => {
+    this.setState({isAlert: !this.state.isAlert, isDangerous: !this.state.isDangerous})
+  }
+
   render() {
+
+    const clazzes = classNames({
+      box: true,
+      alert: this.state.isAlert,
+      dangerous: this.state.isDangerous
+    });
+
     return (
       <div className="App">
         <header className="App-header">
@@ -15,6 +32,8 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className={clazzes}>Style with with classnames()</p>
+        <button onClick={this.changeColor}>Change color</button>
         <Timer/>
       </div>
     );
