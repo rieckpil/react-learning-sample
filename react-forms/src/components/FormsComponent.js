49bd75208793d7ceef3f4f9e6b6475610990f047
Philip Riecks <philip@riecks.io>
2018-08-30 16:28:47 +0200

    add new sample project for forms with react

---
+++ react-forms/src/components/FormsComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class FormsComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Forms with React!</h3>
+
+            </div>
+        );
+    }
+}
+
+export default FormsComponent;
