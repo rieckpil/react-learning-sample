20860bd1a902c1343410f3915cd53e5581e32296
Philip Riecks <philip@riecks.io>
2018-08-31 16:46:46 +0200

    add first test with enzyme

--- react-forms/src/App.test.js
+++ react-forms/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('should have input fields', () => {
+    const wrapper = shallow(
+      <App/>
+    );
+    expect(wrapper.contains( <h1 className="App-title">Welcome to React</h1>)).toBe(true);
 });
---
+++ react-forms/src/tempPolyfills.js
@@ -0,0 +1,4 @@
+const raf = global.requestAnimationFrame = (cb) => {
+    setTimeout(cb, 0);
+};
+export default raf;
